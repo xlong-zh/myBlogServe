8063247d474afdf52f83f02dbf60a1eb4efd53ee
xlong-zh <907737948@qq.com>
2019-12-21 15:31:41 +0800

    12-21

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require("express")
+const express = require('express');
 
-const app = express()
-app.set('secret', 'zxl123')
+const app = express();
+app.set('secret', 'zxl123');
 
-app.use(require('cors')())
-app.use(express.json())
+app.use(require('cors')());
+app.use(express.json());
 
-app.use('/uploads', express.static(__dirname + '/uploads'))
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
-require('./plugins/db.js')(app)
-require('./router/admin/index.js')(app)
-require('./router/web/index.js')(app)
+require('./plugins/db.js')(app);
+require('./router/admin/index.js')(app);
+require('./router/web/index.js')(app);
 
-app.listen(3000, () => {
-  console.log('服务器开启成功http://localhost:3000')
-})
+app.listen(4000, () => {
+  console.log('服务器开启成功http://localhost:4000');
+});
