4cb2c2b193e3161cf10ccb8b4fdb43ecf8fefc63
xlong-zh <907737948@qq.com>
2020-02-09 22:33:48 +0800

    2-9

--- router/web/index.js
+++ router/web/index.js
@@ -1,23 +1,26 @@
 module.exports = app => {
-  const router = require('express').Router()
-  const mongoose = require('mongoose')
+  const router = require('express').Router();
+  const mongoose = require('mongoose');
 
-  const Article = mongoose.model('Article')
-  const Category = mongoose.model('Category')
-  const Image = mongoose.model('Image')
+  const Article = mongoose.model('Article');
+  const Category = mongoose.model('Category');
+  const Image = mongoose.model('Image');
   // const Image = require('../../models/Image')
 
   router.get('/image/list', async (req, res) => {
-    const data = await Image.find()
-    res.send(data)
-  })
+    const data = await Image.find();
+    res.send(data);
+  });
 
   router.get('/article/list', async (req, res) => {
-    const data = await Article.find().populate('category')
-    res.send(data)
-  })
+    const data = await Article.find().populate('category');
+    res.send(data);
+  });
 
+  router.get('/article/list/:id', async (req, res) => {
+    const data = await Article.findById(req.params.id).populate('category');
+    res.send(data);
+  });
 
-  app.use('/web/api', router)
-
-}
+  app.use('/web/api', router);
+};
